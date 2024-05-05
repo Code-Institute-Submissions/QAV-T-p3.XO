fbf25ff40e3b10b4f37fbe5de38cc6a2ec820781
QAV-T S <safieh.tariq@gmail.com>
2024-05-05 20:13:24 +0000

    adding all winning patterns

--- run.py
+++ run.py
@@ -14,6 +14,24 @@ def board(spots):
             else:
               return 'X' 
 
+def check_for_win(spots):
+    # Handle Horizontal Cases
+    if (spots[1] == spots[2] == spots[3]) \
+    or (spots[4] == spots[5] == spots[6]) \
+    or (spots[7] == spots[8] == spots[9]):
+        return True
+    # Handle Vertical Cases
+    elif (spots[1] == spots[4] == spots[7]) \
+    or (spots[2] == spots[5] == spots[8]) \
+    or (spots[3] == spots[6] == spots[9]):
+        return True
+    # Diagonal Cases
+    elif (spots[1] == spots[5] == spots[9]) \
+    or (spots[3] == spots[5] == spots[7]):
+        return True
+    else:
+        return False              
+
 
     # Get input and make sure it's valid
     choice = input()
