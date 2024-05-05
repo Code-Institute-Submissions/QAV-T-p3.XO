08998695e6561d8f03de5575255deca481f4d8b0
QAV-T S <safieh.tariq@gmail.com>
2024-05-05 20:01:41 +0000

    get input from the user and replace it with the assigned spot

--- run.py
+++ run.py
@@ -1,14 +1,28 @@
-#printing the main board
-def board():
+import os
+
+# Displying the main board
+def board(spots):
     board = (f"|{spots[1]}|{spots[2]}|{spots[3]}|\n"
              f"|{spots[4]}|{spots[5]}|{spots[6]}|\n"
              f"|{spots[7]}|{spots[8]}|{spots[9]}|")
     print(board)
 
-    # check turn function 
+    # check player turn function 
     def check_turn(turn):
             if turn % 2 == 0:
               return 'O'
             else:
               return 'X' 
 
+
+    # Get input and make sure it's valid
+    choice = input()
+    if choice == 'q':
+        playing = False
+    elif str.isdigit(choice) and int(choice) in spots:
+        if not spot[int(choice)] in {"X", "O"}: 
+            turn += 1
+            spots[int(choice)] = check_turn(turn) 
+
+
+#def main():
